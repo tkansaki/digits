ffb692d16c40d8e38349be1d68b97a4fa8246793
tkansaki <tkansaki@hawaii.edu>
2021-10-30 23:33:30 -1000

    landing page mockup finished in 10 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <div className={'landing-div'}>
+        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container columns={'3'}>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
+          <Grid.Column>
+            <Icon name={'users'} size={'huge'} inverted />
+            <Header as={'h1'} inverted>Multiple Users</Header>
+            <Header as={'h3'} inverted>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Icon name={'file alternate'} size={'huge'} inverted />
+            <Header as={'h1'} inverted>Contact Details</Header>
+            <Header as={'h3'} inverted>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Icon name={'calendar outline'} size={'huge'} inverted />
+            <Header as={'h1'} inverted>Timestamped Notes</Header>
+            <Header as={'h3'} inverted>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Header>
+          </Grid.Column>
 
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+        </Grid>
+      </div>
     );
   }
 }
